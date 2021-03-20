429b7c6b6cf55ef02aaa8219a815ace01443c7e4
arjun <arjun@cybrillas-air>
2021-03-20 12:25:55 +0530

    Commit Initial Project

---
+++ com.fullcourse/src/main/java/com/fullcourse/com/fullcourse/App.java
@@ -0,0 +1,13 @@
+package com.fullcourse.com.fullcourse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
